db1438d2d47aeb0f8c50b113283539b8122261d5
Daniel Le'Sage <daniellesage25@gmail.com>
2022-06-23 10:40:11 -0600

    chore: adding controller level auth to users and cleaning up other files

--- apps/api/src/users/users.controller.ts
+++ apps/api/src/users/users.controller.ts
@@ -1,25 +1,34 @@
-import { Controller, Get, Param, Post, UseGuards } from '@nestjs/common';
+import {
+    Controller,
+    Get,
+    NotFoundException,
+    Param,
+    Post,
+    UseGuards,
+} from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { UsersService } from './users.service';
 
+@UseGuards(AuthGuard('jwt'))
 @Controller('users')
 export class UsersController {
     constructor(private readonly service: UsersService) {}
 
     @Get()
-    @UseGuards(AuthGuard('jwt'))
     findAll() {
         return this.service.findAll();
     }
 
     @Get('/sub/:sub')
-    @UseGuards(AuthGuard('jwt'))
     findBySub(@Param('sub') sub: string) {
-        return this.service.findBySub(sub);
+        return this.service.findBySub(sub).then((user) => {
+            if (!user)
+                return new NotFoundException(`User with ${sub} not found.`);
+            return user;
+        });
     }
 
     @Post()
-    @UseGuards(AuthGuard('jwt'))
     createUser() {
         return this.service.createUser({
             name: 'dan',
